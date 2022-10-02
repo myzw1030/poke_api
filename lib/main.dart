973b829df9ec79e0bae2b769c04edcf77be2616a
tommy <tmtmtoa@gmail.com>
2022-10-02 20:18:52 +0900

    poke

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
+
+const maxNumber = 385;
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +34,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> imageUrls = [];
+
+  Future<void> fetchImagesUrls() async {
+    for (int index = 1; index <= maxNumber; index++) {
+      final url = 'http://pokeapi.co/api/v2/pokemon/$index';
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        // ステータスコード200はリクエストが成功した場合返されるコード
+        // HTTPリクエストに成功した時の処理
+        var jsonResponse = json.decode(response.body);
+        final imageUrl = jsonResponse['sprites']['front_default'];
+        setState(() {
+          imageUrls.add(imageUrl);
+        });
+      } else {
+        // throw Exception('Failed to fetch image URL');
+        print(response.statusCode);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    imageUrls = [];
+    fetchImagesUrls();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: imageUrls.length,
+        itemBuilder: (context, int index) {
+          return CachedNetworkImage(
+            imageUrl: imageUrls[index],
+            placeholder: (context, url) => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          );
+        },
+      ),
     );
   }
 }
